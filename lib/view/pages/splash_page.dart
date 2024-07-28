84c129a4dd73dd7303367a4a60213412f24cb63c
JeHeeYu <sees1234558@gmail.com>
2024-07-28 14:25:28 +0900

    fix: 스플래시 이미지 전체 적용되도록 수정

--- lib/view/pages/splash_page.dart
+++ lib/view/pages/splash_page.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:oha/models/upload/upload_get_model.dart';
 import 'package:oha/statics/images.dart';
 import 'package:oha/utils/app_initializer.dart';
-import 'package:oha/view_model/my_page_view_model.dart';
-import 'package:oha/view_model/weather_view_model.dart';
-import 'package:oha/view/pages/login_page.dart';
-import 'package:provider/provider.dart';
-
-import '../../app.dart';
-import '../../statics/strings.dart';
-import '../../view_model/location_view_model.dart';
-import '../../view_model/login_view_model.dart';
-import '../../view_model/upload_view_model.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -36,9 +23,13 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SvgPicture.asset(
-        Images.splashBg,
-        fit: BoxFit.cover,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: SvgPicture.asset(
+          Images.splashBg,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
