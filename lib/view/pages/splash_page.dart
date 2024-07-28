ae2512e321511200bafc53fe87342a293d1a7176
JeHeeYu <sees1234558@gmail.com>
2024-07-28 14:59:37 +0900

    feat: 인터넷 연결 끊겼을 때 예외 추가

--- lib/view/pages/splash_page.dart
+++ lib/view/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:oha/statics/images.dart';
 import 'package:oha/utils/app_initializer.dart';
 
@@ -12,12 +13,75 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   @override
   void initState() {
     super.initState();
-    AppInitializer.initialize(context);
+    _checkInternetConnection();
+  }
+
+  Future<void> _checkInternetConnection() async {
+    try {
+      var connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult == ConnectivityResult.none) {
+        _showNoInternetDialog();
+      } else {
+        AppInitializer.initialize(context);
+      }
+    } catch (e) {
+      print("에러 발생 123123: $e");
+      _showErrorDialog(e.toString());
+    }
+  }
+
+  void _showNoInternetDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("인터넷 연결 끊김"),
+          content: const Text("인터넷에 연결되어 있지 않습니다. 앱을 종료합니다."),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("확인"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _closeApp();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("오류 발생"),
+          content: Text("오류가 발생했습니다: $message"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("확인"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _closeApp();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _closeApp() {
+    Future.delayed(const Duration(milliseconds: 100), () {
+      SystemNavigator.pop();
+    });
   }
 
   @override
